5db957c9a2c11897dce744143fc6ecd88afd2e85
Sebastian <sjoerz@skyface.de>
2023-12-25 00:11:39 +0100

    removed print statements

--- mkdocs_quiz/js/quiz.js
+++ mkdocs_quiz/js/quiz.js
@@ -1,16 +1,12 @@
 document.querySelectorAll('.quiz').forEach((quiz) => {
-  console.log(quiz);
   let form = quiz.querySelector('form');
   let fieldset = form.querySelector('fieldset');
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('Submitted');
     let answer = form.querySelector('input[name="answer"]:checked');
-    console.log(answer);
     if (answer) {
       let correct = answer.hasAttribute('correct');
       let section = quiz.querySelector('section');
-      // section.classList.remove('hidden');
       if (correct) {
         section.classList.remove('hidden');
         resetFieldset(fieldset);
